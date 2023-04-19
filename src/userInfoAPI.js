89bac3eec6cea4fccaccbae04f7edad371de1b38
Jacob MacFarlane <jmacfarlane22@turing.edu>
2023-04-19 10:01:46 -0700

    shows the user info class started

---
+++ src/userInfoAPI.js
@@ -0,0 +1,5 @@
+class UserInfo {
+    constructor(userInfo) {
+        this.userInfo = userInfo
+    }
+}
---
+++ test/userInfoAPI-test.js
@@ -0,0 +1,8 @@
+import { expect } from 'chai';
+
+
+describe('See if the tests are running', function() {
+  it('should return true', function() {
+    expect(true).to.equal(true);
+  });
+});
