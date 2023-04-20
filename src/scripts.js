cbcd56a93846eef53a0f79c0d552856b47be4b4e
Jacob MacFarlane <jmacfarlane22@turing.edu>
2023-04-19 17:27:13 -0700

    shows the most basic code being completed in order to plan aka pseudocoding

--- src/scripts.js
+++ src/scripts.js
@@ -25,12 +25,26 @@ function createStartPage() {
         console.log(randomUserId, 'id')
         renderUserInfo()
         renderPastTrips()
-        renderPendingTrips()
-        renderFavoriteTrips()
+        // renderPendingTrips()
+        // renderFavoriteTrips()
     })
 }
 
 function generateRandomId() {
     return Math.floor(Math.random() * allTravelers.userInfo.length)
+}
+
+function renderUserInfo() {
+    const currentUser = allTravelers.getUserById(randomUserId)
+    //will do innerHTML upon Fan Favorite trips to make it say
+    //hello[userName] we think you might like these 3 trips
+    //interpolate upon the three boxes to show 3 destinations they have not been
+    console.log(currentUser, 'current User')
+}
+function renderPastTrips() {
+    const currentUser = allTravelers.getUserById(randomUserId)
+    //in here upon clicking the Previous trips button
+    //all of the previous trips will be added into the display
+    //clicking upcoming or pending should hide them
 }
 console.log('This is the JavaScript entry file - your code begins here.');
