bd9378308299b7a7ed36fbd3ef819e103d8f9f53
Jacob MacFarlane <jmacfarlane22@turing.edu>
2023-04-25 10:56:41 -0700

    shows the dom displaying correctly as well as the prices correct

--- src/scripts.js
+++ src/scripts.js
@@ -208,22 +208,29 @@ function displayPending() {
   })
 pendingGrid.innerHTML = ''
    uniqueTrips.forEach((trip) => {
+    let basePrice = allDestinations.getBaseTotal((trip.destination.estimatedLodgingCostPerDay * trip.duration),(trip.destination.estimatedFlightCostPerPerson * trip.travelers))
+        let sellerFee = allDestinations.getSellerFee((trip.destination.estimatedLodgingCostPerDay * trip.duration),(trip.destination.estimatedFlightCostPerPerson * trip.travelers))   
        const existingTrip = document.querySelector(`.tripCont.pending[data-place-id="${trip.placeId}"]`);
       console.log(existingTrip, 'exist')
       console.log(trip, 'trip-ski')
       if (!trip.id && !pendingGrid.innerHTML.includes(trip.destination.destination)) {
            pendingGrid.innerHTML += `
            <div class="tripCont pending">
-           <div class="tripList">
-             <a> <img src="${trip.destination.image}" alt="${trip.destination.alt}"class="thumbnail"></a>
-             <div class="flexDiv">
-               <div class="tripInfo">
-                 <a class="favLocation">${trip.destination.destination}</a>
-                 <p class="pricePerNight totalPricePerNight"> Total Lodging Cost: ${allDestinations.getTotalLodgingDuration(trip.placeId, trip.duration)}$</p>
-                 <p class="roundTripPrice">Total Flight Cost: ${allDestinations.getTotalFlightCost(trip.placeId, trip.travelers)}$</p>
-               </div>
-             </div>
-           </div>
+                <div class="tripList">
+                    <a> <img src="${trip.destination.image}" alt="${trip.destination.alt}"class="thumbnail"></a>
+                    <div class="flexDiv">
+                        <div class="tripInfo">
+                            <a class="favLocation">${trip.destination.destination}</a>
+                            <p class="pricePerNight totalPricePerNight"> Total Lodging Cost: ${allDestinations.getTotalLodgingDuration(trip.placeId, trip.duration)}$</p>
+                            <p class="roundTripPrice">Total Flight Cost: ${allDestinations.getTotalFlightCost(trip.placeId, trip.travelers)}$</p>
+                        </div>
+                        <div class="tripPrice">
+                            <p class="baseTotal"> Base Price: ${basePrice}$</p>
+                            <p class="sellerFee"> 10% Seller Fee: ${sellerFee}$</p>
+                            <p class="fullTotal"> Estimated Total: ${allDestinations.getEstimatedTotal(basePrice, sellerFee)}$</p>
+                        </div>
+                    </div>
+                </div>
          </div>`
        }
 
@@ -245,19 +252,26 @@ function renderCurrentPastTrips() {
    })
    tripGrid.innerHTML = ''
    trips.forEach((trip) => {
+        let basePrice = allDestinations.getBaseTotal((trip.destination.estimatedLodgingCostPerDay * trip.duration),(trip.destination.estimatedFlightCostPerPerson * trip.travelers))
+        let sellerFee = allDestinations.getSellerFee((trip.destination.estimatedLodgingCostPerDay * trip.duration),(trip.destination.estimatedFlightCostPerPerson * trip.travelers))
        if (!trip.id && !tripGrid.innerHTML.includes(trip.destination.destination)) {
        tripGrid.innerHTML += `
        <div class="tripCont pending">
-       <div class="tripList">
-         <a> <img src="${trip.destination.image}" alt="${trip.destination.alt}"class="thumbnail"></a>
-         <div class="flexDiv">
-           <div class="tripInfo">
-             <a class="favLocation">${trip.destination.destination}</a>
-             <p class="pricePerNight totalPricePerNight"> Total Lodging Cost: ${allDestinations.getTotalLodgingDuration(trip.placeId, trip.duration)}$</p>
-             <p class="roundTripPrice">Total Flight Cost: ${allDestinations.getTotalFlightCost(trip.placeId, trip.travelers)}$</p>
-           </div>
-         </div>
-       </div>
+            <div class="tripList">
+                <a> <img src="${trip.destination.image}" alt="${trip.destination.alt}"class="thumbnail"></a>
+                <div class="flexDiv">
+                    <div class="tripInfo">
+                        <a class="favLocation">${trip.destination.destination}</a>
+                        <p class="pricePerNight totalPricePerNight"> Total Lodging Cost: ${allDestinations.getTotalLodgingDuration(trip.placeId, trip.duration)}$</p>
+                        <p class="roundTripPrice">Total Flight Cost: ${allDestinations.getTotalFlightCost(trip.placeId, trip.travelers)}$</p>
+                    </div>
+                    <div class="tripPrice">
+                        <p class="baseTotal"> Base Price: ${basePrice}$</p>
+                        <p class="sellerFee"> 10% Seller Fee: ${sellerFee}$</p>
+                        <p class="fullTotal"> Estimated Total: ${allDestinations.getEstimatedTotal(basePrice, sellerFee)}$</p>
+                    </div>
+                </div>
+            </div>
      </div>
        `
        allTimeMoney.innerText = `${allTrips.allTimeSpending(currentUserId)} Dollars`
